c4f58d7d01352b2577fbb4f831cd9daf840513ce
gedyeyasu <gedyeyasu@gmail.com>
2020-05-21 09:50:25 +0300

    built frontend functionality

--- app.py
+++ app.py
@@ -6,21 +6,42 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-	return render_template("index.html")
-
-
-@app.route("/rates")
-def rates():
-
 	#value to be sent to form
-	res= requests.get("http://data.fixer.io/api/latest?access_key=23bd78a271bd8f29985a755297f0560a")
+	res= requests.get("http://data.fixer.io/api/symbols?access_key=23bd78a271bd8f29985a755297f0560a")
 	data=res.json()
-	base=data["symbols"]
-
-
+	currencies=data["symbols"]
+	
+	
 	#returned value from the form
-	base_syb=request.from.get("base_syb")
-	xch_syb=request.from.get("xch_syb")
-	base_val=request.from.get("base_val")
+	base_syb=request.form.get("base_syb")
+	xch_syb=request.form.get("target_syb")
+	base_val=request.form.get("amount")
 
-	return render_template("rates.html")	
+
+	return render_template("index.html", currencies=currencies)
+
+
+@app.route("/convert", methods=["POST", "GET"])
+def convert():
+
+	#getting value from form
+	base_syb=str(request.form.get("base_syb"))
+	xch_syb=str(request.form.get("target_syb"))
+	base_val=request.form.get("amount")
+
+		
+
+	#converting in to target currency
+	res2=requests.get("https://api.exchangeratesapi.io/latest?base=USD&symbols=xch_syb HTTP/1.1")
+	ret=res2.json()
+	return(str(ret)) # just to return error
+	if ret["success"]:
+		result=ret["result"]
+	else:
+		return("Api request error") # just to return error
+
+
+	return render_template("index.html", result=result)	
+
+if __name__=='__main__':
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import requests
 
 def main():
-	res=requests.get("http://data.fixer.io/api/latest?access_key=23bd78a271bd8f29985a755297f0560a")
+	res=requests.get("http://data.fixer.io/api/symbols?access_key=23bd78a271bd8f29985a755297f0560a")
+	
+	li={'adam':'boy', 'alice':'girl', }
 	data=res.json()
-
 	base=data["symbols"]
-	print(base)
 
+	res2=requests.get("http://data.fixer.io/api/convert?access_key=23bd78a271bd8f29985a755297f0560a& from = base_syb& to = xch_syb& amount = base_val")
+	ret=res2["result"]
+	
+
+	for items in base.values:
+		print(items)
+
+
+	#	for item in data:
+	#	print("with for lopp")
+	#for lists in base:
+	#	print(lists)
 
 
 if __name__ == '__main__':
